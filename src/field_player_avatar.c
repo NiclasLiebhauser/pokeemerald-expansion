0b49e6d676196fcec0ac13880a3ea6b04ca93199
Hodron <niclas@liebhauser.eu>
2024-07-09 03:01:43 +0200

    Enable dashing while surfing

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -631,8 +631,11 @@ static void PlayerNotOnBikeMoving(u8 direction, u16 heldKeys)
 
     if (gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_SURFING)
     {
-        // same speed as running
-        PlayerWalkFast(direction);
+        if((heldKeys & B_BUTTON) && FlagGet(FLAG_SYS_B_DASH))
+            PlayerRideWaterCurrent(direction);
+        else
+            PlayerWalkFast(direction);
+        
         return;
     }
 
