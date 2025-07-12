27d0e605f309242f5e14c012f8db872dce0c4e8d
Hodron <niclas@liebhauser.eu>
2025-07-12 21:11:10 +0200

    Add Nightshade Horror version

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1159,3 +1159,5 @@ EventScript_VsSeekerChargingDone::
 	.include "data/scripts/dexnav.inc"
 
 	.include "data/maps/NightshadeTown/scripts.inc"
+
+	.include "data/maps/NightshadeTown_Horror/scripts.inc"
