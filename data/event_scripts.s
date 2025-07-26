e6e01af1fc1dc585434e8882e10fa6e9f78dc91e
Hodron <niclas@liebhauser.eu>
2025-07-26 14:03:02 +0200

    Add Inn map

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1195,3 +1195,11 @@ EventScript_VsSeekerChargingDone::
 	.include "data/maps/NightshadeTown_Horror_MansionF1/scripts.inc"
 
 	.include "data/maps/NightshadeTown_Horror_MansionF2/scripts.inc"
+
+	.include "data/maps/NightshadeTown_Inn_1F/scripts.inc"
+
+	.include "data/maps/NightshadeTown_Inn_2F/scripts.inc"
+
+	.include "data/maps/NightshadeTown_Horror_Inn_1F/scripts.inc"
+
+	.include "data/maps/NightshadeTown_Horror_Inn_2F/scripts.inc"
